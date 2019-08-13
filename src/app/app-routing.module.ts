f9dfa0b849ab464a4fb96d5ad27a235252cd9c2a
Rok Spacapan <rok.spacapan@sledat.com>
2019-08-13 16:39:02 +0200

    add user-detail presentational component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
-<<<<<<< HEAD
-import { UsersTableComponent } from "./users-table/users-table.component";
-
-export const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'users-table', component: UsersTableComponent}
-=======
-import { UsersComponent } from './users/users.component';
-import { UsersTableComponent } from "./users-table/users-table.component";
-import { UserComponent }  from "./user/user.component";
+import { UsersTableComponent } from './users-table/users-table.component';
+import { UserComponent } from './user/user.component'
 
 export const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'users-table', component: UsersTableComponent},
-  {path: 'users', component: UsersComponent},
   {path: 'user/:id', component: UserComponent}
->>>>>>> dev-ahead
 ];
 
 @NgModule({
